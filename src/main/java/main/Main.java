0d177ad5c697a0d1cd27ad9927431e3106216a6b
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-03 23:21:59 +0400

    added to context some beans of different types

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,18 @@
 package main;
 
+import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext();
-        Parrot p = new Parrot();
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+
+        var s = context.getBean(String.class);
+        System.out.println(s);
+
+        var n = context.getBean(Integer.class);
+        System.out.println(n);
     }
 }
