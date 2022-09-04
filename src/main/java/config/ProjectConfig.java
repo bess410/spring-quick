a1e18ede13915bd8451ad43d7d2c145048be0d9d
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-04 15:22:23 +0400

    added Component and ComponentScan annotations

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,42 +1,10 @@
 package config;
 
-import main.Parrot;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan(basePackages = "main")
 public class ProjectConfig {
 
-    @Bean(value = "koko")
-    Parrot parrot1() {
-        var p = new Parrot();
-        p.setName("Koko");
-        return p;
-    }
-
-    @Primary
-    @Bean(name = "miki")
-    Parrot parrot2() {
-        var p = new Parrot();
-        p.setName("Miki");
-        return p;
-    }
-
-    @Bean("riki")
-    Parrot parrot3() {
-        var p = new Parrot();
-        p.setName("Riki");
-        return p;
-    }
-
-    @Bean
-    String hello() {
-        return "Hello";
-    }
-
-    @Bean
-    Integer ten() {
-        return 10;
-    }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,12 +7,7 @@ public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         var p = context.getBean(Parrot.class);
+        System.out.println(p);
         System.out.println(p.getName());
-
-        var s = context.getBean(String.class);
-        System.out.println(s);
-
-        var n = context.getBean(Integer.class);
-        System.out.println(n);
     }
 }
