c738e751168f0a71d184718fcc6a5bab86a65e7d
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-04 15:41:11 +0400

    added bean to context programmatically

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,11 +3,19 @@ package main;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        var p = context.getBean(Parrot.class);
-        System.out.println(p);
+        Parrot x = new Parrot();
+        x.setName("Kiki");
+
+        Supplier<Parrot> parrotSupplier = () -> x;
+
+        context.registerBean("parrot1", Parrot.class, parrotSupplier);
+
+        Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
     }
 }
--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -2,16 +2,9 @@ package main;
 
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
 public class Parrot {
     private String name;
-
-    @PostConstruct
-    public void init() {
-        this.name = "Kiki";
-    }
 
     public String getName() {
         return name;
