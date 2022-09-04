1f175252b0b94837a3e1ddfb37e275b9ec82c237
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-04 23:02:51 +0400

    wiring through method's parameter

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -11,15 +11,15 @@ public class ProjectConfig {
     @Bean
     public Parrot parrot() {
         Parrot p = new Parrot();
-        p.setName("Kiki");
+        p.setName("Koko");
         return p;
     }
 
     @Bean
-    public Person person() {
+    public Person person(Parrot parrot) {
         Person p = new Person();
         p.setName("Ella");
-        p.setParrot(parrot());
+        p.setParrot(parrot);
         return p;
     }
 }
